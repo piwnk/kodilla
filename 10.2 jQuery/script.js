a273178dc27139138d64264b740cda923e3c3aac
piotr <pyhivo@gmail.com>
2017-09-12 19:12:16 +0200

    10.2 ready

--- 10.2 jQuery/script.js
+++ 10.2 jQuery/script.js
@@ -1,6 +1,44 @@
-$(function(){
-   var carousel = $('#carousel');
-   carousel.find('img').each(function(index, element) {
-      $(element).attr('src', `http://lorempixel.com/80${index}/60${index}/`);
-   });
+$(function () {
+   cloneDivs();
+   addColor();
+   addButtons();
+   addListener();
+
 });
+
+const cloneDivs = () => {
+   let $container = $('.container');
+   let $div1 = $('.div1');
+   let $div2 = $('.div2');
+   for (let i in Array.from(Array(5).keys())) {
+      $container.append($div1.clone());
+      $container.append($div2.clone());
+   }
+};
+
+const addColor = () => {
+   let $span = $("span");
+   $span.each(function (index, element) {
+      if (index % 2 === 0) {
+         $(element).css('color', 'red');
+      }
+   });
+};
+
+const addButtons = () => {
+   let $p = $("p");
+   $p.each((i, e) => {
+      // p.each(function(i, e) {
+      // var button = '<button class="btn" data-tmp="' + i + '">Click me</button>';
+      let button = `<button class="btn" data-tmp="${i}">Click me</button>`;
+      $(e).append(button);
+   });
+};
+
+const addListener = () => {
+   $('button').click((e) => {
+      alert($(e.currentTarget).attr('data-tmp'));
+      // $('button').click(function () {
+      //    alert($(this).attr('data-tmp'));
+   });
+};
