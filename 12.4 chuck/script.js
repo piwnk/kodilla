2f644cce878930b021f90b3572b8780e0dd3ac9b
piotr <pyhivo@gmail.com>
2017-09-20 20:52:20 +0200

    12.4 chuck init

---
+++ 12.4 chuck/script.js
@@ -0,0 +1,20 @@
+const url = 'http://api.icndb.com/jokes/random';
+
+const button = document.getElementById("get-joke");
+const paragraph = document.getElementById("joke");
+
+button.addEventListener("click", function() {
+   getJoke();
+});
+
+
+
+function getJoke() {
+   let xhr = new XMLHttpRequest();
+   xhr.open("GET", url);
+   xhr.addEventListener("load", function () {
+      let response = JSON.parse(xhr.response);
+      paragraph.innerHTML = response.value.joke;
+   });
+   xhr.send();
+}
