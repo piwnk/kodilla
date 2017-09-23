8f2a72cb2eeaf7cdcd81fe8be28f6f0c12c4c2d2
piotr <pyhivo@gmail.com>
2017-09-23 07:23:13 +0200

    quote machine working

---
+++ 12.5 quote machine/script.js
@@ -0,0 +1,53 @@
+$(function () {
+
+
+   const tweetLink = "https://twitter.com/intent/tweet?text=";
+   const quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+   // const quoteUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parseQuote";
+
+
+   function getQuote() {
+      // $.getJSON(quoteUrl, createTweet);
+   
+      $.ajax({
+         url: quoteUrl,
+         success: createTweet,
+         cache: false
+      });
+   }
+
+   function createTweet(input) {
+      let data = input[0];
+      console.log(data);
+
+      let quoteText = $(data.content).text().trim();
+      // let quoteText = data.quoteText;
+      let quoteAuthor = data.title;
+      // let quoteAuthor = data.quoteAuthor;
+
+      if (!quoteAuthor.length) {
+         quoteAuthor = "Unknown";
+      }
+
+      const tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+      // LOOP FOR VALID QUOTE
+      if (tweetText.length > 140) {
+         getQuote();
+      } else {
+         console.log(tweetText.length);
+
+         let tweet = tweetLink + encodeURIComponent(tweetText);
+         $('.quote').text(quoteText);
+         $('.author').text("Author: " + quoteAuthor);
+         $('.tweet').attr('href', tweet);
+      }
+
+   }
+
+   getQuote();
+   $(".trigger").click(function () {
+      getQuote();
+   });
+
+});
